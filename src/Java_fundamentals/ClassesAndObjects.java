99a4a4ace5fa8f305cc95f132f4cfd6241a68466
Chetan Adari. <c.adari@dsslp.com>
2025-01-22 12:38:33 +0530

    created a new package with a class called ClassesAndObjects

---
+++ src/Java_fundamentals/ClassesAndObjects.java
@@ -0,0 +1,11 @@
+package Java_fundamentals;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class ClassesAndObjects {
+    public static void main(String[] args) {
+
+        System.out.println("so this is my main class");
+        
+    }
+}
